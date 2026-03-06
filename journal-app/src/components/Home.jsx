8adf8a3f51dff39b8cb0bbcfbd8c7cd82bbb4417
Elina Liault <elina.liault@gmail.com>
2026-03-06 10:26:15 +0100

    Update useEffect variables dependencies

--- journal-app/src/components/Home.jsx
+++ journal-app/src/components/Home.jsx
@@ -64,7 +64,7 @@ export default function Home() {
 
         // Cleanup
         return () => clearInterval(animInterval);
-    }, [screenRef, logoRef]);
+    }, [screenRef, mainRef, logoRef]);
 
     return (
     <>
