b188224c1b19fadf73914e6092f44ed13c997260
Elina Liault <elina.liault@gmail.com>
2026-03-18 16:53:11 +0100

    Conclu text, style et courbe d'apprentissage

--- journal-app/src/components/articles/Conclu.jsx
+++ journal-app/src/components/articles/Conclu.jsx
@@ -1,4 +1,5 @@
 import ButtonNext from "../ButtonNext";
+import courbeApprentissage from '../../assets/courbe-apprentissage.png'
 
 export default function Conclu() {
     return (
@@ -7,9 +8,19 @@ export default function Conclu() {
         <p>Ce journal d'apprentissage m'a donné l'occasion de m'intéresser à des compétences que je voulais apprendre
             depuis un moment déjà. Il m'a ainsi permis de réaliser ce site, pas à pas, en consolidant les compétences 
             déjà acquises et en apprenant toutes celles qui me semblait nécessaire pour arriver à faire ce que je voulais faire. 
-            Ce projet m'a aussi aidé à consolider ma capacité à apprendre par moi-même.
         </p>
-        <p>Je suis contente du résultat, et ce n'est que le début ! 
+        <p>Ci-dessous, la représentation graphique de mon apprentissage dans le temps :
+        <img src={courbeApprentissage} className="courbeLearning" alt="Courbes de mes apprentissages" />
+        </p>
+        <p>
+            Je considère que j'ai réussi à apprendre les compétences que je cherchais à maîtriser et le niveau que je cherchais à atteindre.
+            De même, l'organisation que j'ai suivie me parait tout particulièrement appropriée, car j'ai suivi la manière dont mon 
+            cerveau fonctionne et souhaitait apprendre. Cela m'a notamment permis de relever que j'ai souvent besoin de comprendre
+            pourquoi je fais quelque chose avant de le faire, d'en comprendre le fonctionnement et les mécaniques.
+            Ce projet m'a aussi aidé à consolider ma capacité à apprendre par moi-même. 
+            <br />C'est pourquoi je suis particulièrement contente du rendu et du résultat de ce projet.
+        </p>
+        <p>Et ce n'est que le début ! 
             <br />J'ai hâte de pouvoir mettre ces nouvelles compétences
             au service de la réalisation de mon portfolio et bien d'autres projets, je l'espère.
         </p>
