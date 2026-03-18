b49999df2367814c17e4f09c1d68c04c126cbf97
Elina Liault <elina.liault@gmail.com>
2026-03-18 13:03:55 +0100

    Text and style Article Conclu

--- journal-app/src/components/articles/Conclu.jsx
+++ journal-app/src/components/articles/Conclu.jsx
@@ -4,7 +4,15 @@ export default function Conclu() {
     return (
     <>
         <h1>Conclusion</h1>
-        <p>Ici est la conclusion...........</p>
+        <p>Ce journal d'apprentissage m'a donné l'occasion de m'intéresser à des compétences que je voulais apprendre
+            depuis un moment déjà. Il m'a ainsi permis de réaliser ce site, pas à pas, en consolidant les compétences 
+            déjà acquises et en apprenant toutes celles qui me semblait nécessaire pour arriver à faire ce que je voulais faire. 
+            Ce projet m'a aussi aidé à consolider ma capacité à apprendre par moi-même.
+        </p>
+        <p>Je suis contente du résultat, et ce n'est que le début ! 
+            <br />J'ai hâte de pouvoir mettre ces nouvelles compétences
+            au service de la réalisation de mon portfolio et bien d'autres projets, je l'espère.
+        </p>
         <ButtonNext />
     </>
     )
